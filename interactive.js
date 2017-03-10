e5a7124f95d4c67e7ac3d1f0e5240bb0e33dc656
Vincent Liok <vliok@stuy.edu>
2017-03-09 22:04:40 -0500

    move is kinda iffy

--- interactive.js
+++ interactive.js
@@ -4,6 +4,8 @@ var svg = document.getElementById("s");
 var move = document.getElementById("move");
 var clear = document.getElementById("clear");
 
+var rid = 0;
+
 function circleListener(e) {
     if (this.getAttribute("fill") == "red") {
         this.setAttribute("fill", "purple");
@@ -12,7 +14,7 @@ function circleListener(e) {
         this.remove();
         var x = Math.floor(Math.random() * (+svg.getAttribute("width")+1));
         var y = Math.floor(Math.random() * (+svg.getAttribute("height")+1));
-        drawCircle(x,y,20);
+        drawCircle(x,y,40);
     }
     e.stopPropagation();
 }
@@ -33,12 +35,32 @@ function newCircle(e) {
     var x = e.offsetX;
     var y = e.offsetY;
 
-    drawCircle(x, y, 20);
+    drawCircle(x, y, 40);
 }
 
 function bounceCircles(e) {
-    console.log("bounce");
-    // magic 
+    window.cancelAnimationFrame( rid );
+    function move(circle) {
+	var xspeed = 2;
+	var yspeed = 2;
+	function move2() {
+	    var x = parseInt(circle.getAttribute("cx"));
+	    var y = parseInt(circle.getAttribute("cy"));
+	    if (x >= parseInt(svg.getAttribute("width")) - parseInt(circle.getAttribute("r")) || x <= parseInt(circle.getAttribute("r"))) {
+		xspeed = -xspeed;
+	    }
+            if (y >= parseInt(svg.getAttribute("height")) - parseInt(circle.getAttribute("r")) || y <= parseInt(circle.getAttribute("r"))) {
+                yspeed = -yspeed;
+	    }
+	    circle.setAttribute("cx",x + xspeed);
+	    circle.setAttribute("cy",y + yspeed);
+	    rid = window.requestAnimationFrame( move2 );
+	}
+	move2();
+    }
+    for (i = 1; i < svg.childNodes.length; i++){
+	move(svg.childNodes[i]);
+    }
 }
 
 function clearSVG(e) {
