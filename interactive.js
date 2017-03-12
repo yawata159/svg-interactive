96ed1fd463b79a0b7ca156877c2c01d5358a7a79
yawata159 <seijiyawata@gmail.com>
2017-03-11 20:33:01 -0500

    shiny new stuff

--- interactive.js
+++ interactive.js
@@ -1,11 +1,34 @@
 var ns = "http://www.w3.org/2000/svg";
-
 var svg = document.getElementById("s");
 var move = document.getElementById("move");
 var clear = document.getElementById("clear");
 var stop = document.getElementById("stop");
 
 var rid = 0;
+
+function newCircle(e) {
+    regularCircle(e.offsetX, e.offsetY);
+}
+
+function regularCircle(x,y) {
+    drawCircle(30, x, y, 1, 1);
+}
+
+function drawCircle(r,x,y,xdir,ydir) {
+    var ret = document.createElementNS(ns, "circle");
+    ret.setAttribute("cx", x);
+    ret.setAttribute("cy", y);
+    ret.setAttribute("r", r);
+    ret.setAttribute("fill", "red");
+
+    ret.xdir = xdir;
+    ret.ydir = ydir;
+
+    ret.addEventListener("click", circleListener);
+
+    svg.appendChild(ret);
+    return ret;
+}
 
 function circleListener(e) {
     if (this.getAttribute("fill") == "red") {
@@ -15,67 +38,56 @@ function circleListener(e) {
         this.remove();
         var x = Math.floor(Math.random() * (+svg.getAttribute("width")+1));
         var y = Math.floor(Math.random() * (+svg.getAttribute("height")+1));
-        drawCircle(x,y);
+        regularCircle(x, y);
     }
     e.stopPropagation();
 }
 
-function drawCircle(x,y) {
-    var ret = document.createElementNS(ns, "circle");
-    ret.setAttribute("cx", x);
-    ret.setAttribute("cy", y);
-    ret.setAttribute("r", 20);
-    ret.setAttribute("fill", "red");
-
-    ret.xdir = 1;
-    ret.ydir = 1;
-
-    ret.addEventListener("click", circleListener);
-
-    svg.appendChild(ret);
-}
-
-function newCircle(e) {
-    var x = e.offsetX;
-    var y = e.offsetY;
-
-    drawCircle(x, y);
-}
-
 function bounceCircles(e) {
     window.cancelAnimationFrame(rid);
-
     function move() {
         var circles = svg.children;
-    
         for (var i = 0; i < circles.length; i++) {
             var circle = circles[i];
+
             var x = +circle.getAttribute("cx");
             var y = +circle.getAttribute("cy");
             var r = +circle.getAttribute("r");
+            var width = +svg.getAttribute("width");
 
-
-            if (x <= r) circle.xdir = 1;
-            else if (x >= svg.getAttribute("width") - r) circle.xdir = - 1;
-
-            if (y <= r) circle.ydir =  1;
-            else if (y >= svg.getAttribute("height") - r) circle.ydir =  - 1;
-
-            circle.setAttribute("cx", x + +circle.xdir);
-            circle.setAttribute("cy", y + +circle.ydir);
-
+            if (x == width/2) {
+                circle.setAttribute("r", r/2);
+                var duplicate = drawCircle(r/2 , x, y, -circle.xdir, -circle.ydir);
+                nextFrame(duplicate);
+            }
+            nextFrame(circle);
         }
-
         rid = window.requestAnimationFrame(move);
     }
     move()
+}
 
+function nextFrame(circle) {
+    var x = +circle.getAttribute("cx");
+    var y = +circle.getAttribute("cy");
+    var r = +circle.getAttribute("r");
+    var width = +svg.getAttribute("width");
+    var height = +svg.getAttribute("height");
+
+    if (x <= r) circle.xdir = Math.abs(circle.xdir);
+    else if (x >= +width - r) circle.xdir = -Math.abs(circle.xdir);
+
+    if (y <= r) circle.ydir =  Math.abs(circle.xdir);
+    else if (y >= +height - r) circle.ydir =  -Math.abs(circle.xdir);
+
+    circle.setAttribute("cx", x + +circle.xdir);
+    circle.setAttribute("cy", y + +circle.ydir);
+
+    if (r <= 1) circle.remove();
 }
 
 function clearSVG(e) {
-    while (svg.firstChild) {
-        svg.removeChild(svg.firstChild);
-    }
+    while (svg.firstChild) svg.removeChild(svg.firstChild);
 }
 
 function stopIt(e) {
